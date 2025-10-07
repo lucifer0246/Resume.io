c1f069580ff7e0ae151e79ed881c642ec5b7d827
lucifer0246 <knightrider22017@gmail.com>
2025-10-07 15:43:54 +0530

    cookie-issue fixed

--- server/controller/auth-controller.js
+++ server/controller/auth-controller.js
@@ -12,7 +12,7 @@ const generateToken = (id) => {
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "lax", // allows localhost cookies
+  sameSite: "none", // allows localhost cookies
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
 
